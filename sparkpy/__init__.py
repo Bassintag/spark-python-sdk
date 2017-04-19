b087eeab7bead92879803e14ed12209665ed4c88
Antoine S <antoine.stempfer@epitech.eu>
2017-04-19 22:40:14 +0200

    First commit

---
+++ sparkpy/__init__.py
@@ -0,0 +1,15 @@
+#
+#  __init__.py for spark-python-sdk
+#  Started on 18/04/2017 by Antoine
+#
+
+from Spark import Spark
+from Membership import Membership
+from Message import Message
+from Organization import Organization
+from Person import Person
+from Role import Role
+from Room import Room
+from Team import Team
+from TeamMembership import TeamMembership
+from Webhook import Webhook
